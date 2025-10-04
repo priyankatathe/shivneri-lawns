6791d5087373f2a129496ef3885f1f8b9f9c6bab
pooja <https://github.com/poojatayde9960/ecom.git>
2025-10-04 14:57:51 +0530

    kkkkkkkk

--- client/src/formList/Form.jsx
+++ client/src/formList/Form.jsx
@@ -149,6 +149,7 @@ const Form = () => {
                 >
 
                     {/* lg:w-[318%] md:w-[209%] w-full overflow-hidden */}
+                    {/* lg:w-[318%] md:w-[209%] w-full overflow-hidden */}
 
                     <UserInfo formik={formik} />
                     <div className="grid lg:mt-0   md:mt-80 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  overflow-hidden lg:col-span-3">
